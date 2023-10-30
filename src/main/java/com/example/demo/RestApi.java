cd423997b935b78e3897f6e72651d161cbd9013d
oumaima1115 <oumaima.ayachi@esprit.tn>
2023-10-30 17:47:46 +0100

    Add sort

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -1,25 +1,35 @@
 package com.example.demo;
 
+
 import java.io.OutputStream;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class RestApi {
 
     Model model = JenaEngine.readModel("data/inclusify.owl");
 
-    @GetMapping("/events")
+    @GetMapping("/eventsOnline")
     @CrossOrigin(origins = "http://localhost:3000")
-    public String afficherEvents() {
+    public String afficherEventsOnline() {
         String NS = "";
         if (model != null) {
             NS = model.getNsPrefixURI("");
             Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
-            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Event.txt");
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Online.txt");
             System.out.println(res);
             return res.toString();
         } else {
@@ -27,53 +37,64 @@ public class RestApi {
         }
     }
 
-    @GetMapping("/eventsByElement")
+    @GetMapping("/eventsOnsite")
     @CrossOrigin(origins = "http://localhost:3000")
-    public String getEventsByElement(@RequestParam("searchTerm") String searchTerm, @RequestParam("searchCriteria") String searchCriteria) {
+    public String afficherEventsOnsite() {
+        String NS = "";
         if (model != null) {
-            String NS = model.getNsPrefixURI("");
-            String sparqlQuery = generateSPARQLQuery(searchTerm, searchCriteria);
+            NS = model.getNsPrefixURI("");
             Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
-            OutputStream res = JenaEngine.executeQueryFile(inferedModel, sparqlQuery);
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Onsite.txt");
             System.out.println(res);
-            System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
-            return (res==null)?res.toString():null;
+            return res.toString();
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
+
+    @GetMapping("/eventSearch")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String showEventSearchSort(@RequestParam(name = "title", required = false, defaultValue = "") String title,
+                                        @RequestParam(name = "description", required = false, defaultValue = "") String description,
+                                        @RequestParam(name = "sortBy", required = false, defaultValue = "") String sortBy) {
+
+        String sparqlQuery = "PREFIX ns: <http://www.semanticweb.org/rayenbourguiba/ontologies/2023/9/untitled-ontology-4#> " +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "SELECT * " +
+                "WHERE { " +
+                "?online rdf:type ns:Online; " +
+                "        ns:title ?title; " +
+                "        ns:description ?description; " +
+                "        ns:date ?date; " +
+                "        ns:address ?address; " +
+                "        ns:capacity ?capacity; ";
+
+        if (title != null && !title.isEmpty()) {
+            sparqlQuery += " FILTER(regex(?title, \"" + title + "\", \"i\"))";
+        }
+        if (description != null && !description.isEmpty()) {
+            sparqlQuery += " FILTER(regex(?description, \"" + description + "\", \"i\"))";
+        }
+
+// ORDER BY clause should be placed after all FILTER conditions
+        if (sortBy != null && !sortBy.isEmpty()) {
+            if ("capacity".equals(sortBy)) {
+                sparqlQuery += " } ORDER BY ?capacity";
+            } else if ("date".equals(sortBy)) {
+                sparqlQuery += " } ORDER BY ?date";
+            }
+        }
+
+        sparqlQuery += " }"; // Close the SPARQL query
+        if (model != null) {
+            Model inferredModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+            OutputStream res = JenaEngine.executeQuery(inferredModel, sparqlQuery);
+            System.out.println(res);
+            return res.toString();
         } else {
             return "Error when reading model from ontology";
         }
     }
-
-    private String generateSPARQLQuery(String searchTerm, String searchCriteria) {
-        String baseQuery = "PREFIX ns: <http://www.semanticweb.org/rayenbourguiba/ontologies/2023/9/untitled-ontology-4#>\n" +
-                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                "SELECT DISTINCT ?event ?title ?description ?date ?address ?capacity ?eventType\n" +
-                "WHERE {\n" +
-                "  ?event rdf:type ns:OnlineEvent;\n" +
-                "         ns:title ?title;\n" +
-                "         ns:description ?description;\n" +
-                "         ns:date ?date;\n" +
-                "         ns:address ?address;\n" +
-                "         ns:capacity ?capacity.\n" +
-                "  BIND(\"Online Event\" AS ?eventType)\n" +
-                "  FILTER (CONTAINS(LCASE(?title), LCASE(?searchTerm)) ||\n" +
-                "          CONTAINS(LCASE(?description), LCASE(?searchTerm)) ||\n" +
-                "          CONTAINS(LCASE(?capacity), LCASE(?searchTerm)));\n";
-
-        if ("title".equalsIgnoreCase(searchCriteria)) {
-            searchTerm = "\"" + searchTerm + "\"";
-        } else if ("description".equalsIgnoreCase(searchCriteria)) {
-            searchTerm = "\"" + searchTerm + "\"";
-        } else if ("capacity".equalsIgnoreCase(searchCriteria)) {
-            searchTerm = "\"" + searchTerm + "\"";
-        }
-
-        baseQuery = baseQuery.replace("?searchTerm", searchTerm);
-        baseQuery += "}";
-        return baseQuery;
-    }
-
-
-
 
     @GetMapping("/chaud")
     @CrossOrigin(origins = "http://localhost:3000")
@@ -130,6 +151,34 @@ public class RestApi {
         }
     }
 
+    @GetMapping("/JobsList")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String getJobs() {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            // query on the model after inference
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Job.txt");
+//            OutputStream res2 =  JenaEngine.executeQueryFile(inferedModel, "data/query_OrigineVegetale.txt");
+//            OutputStream res3 =  JenaEngine.executeQueryFile(inferedModel, "data/query_Liquide.txt");
+
+//            String res = res1.toString() + res2.toString() + res3.toString() ;
+
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+
+    }
 
 
 
@@ -214,6 +263,150 @@ public class RestApi {
         }
     }
 
+    @GetMapping("/product")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String showProduct() {
+        String NS = "";
+        if (model != null) {
+            NS = model.getNsPrefixURI("");
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_product.txt");
+            System.out.println(res);
+            return res.toString();
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
+
+    @GetMapping("/productsearch")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String showProductSearch(@RequestParam("productName") String productName) {
+        String NS = "";
+        if (model != null) {
+            NS = model.getNsPrefixURI("");
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+            OutputStream res = JenaEngine.executeQueryFileParams(inferedModel, "data/query_product_name_search.txt",productName);
+            System.out.println(res);
+            return res.toString();
+        } else {
+            return "Error when reading model from ontology";
+        }
+    }
+
+    @GetMapping("/productsearchsort")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String showProductSearchSort(@RequestParam(name = "productName", required = false, defaultValue = "") String productName,
+                                        @RequestParam(name = "type", required = false, defaultValue = "") String type,
+                                        @RequestParam(name = "sortBy", required = false, defaultValue = "") String sortBy) {
+
+        String sparqlQuery = "PREFIX ns: <http://www.semanticweb.org/rayenbourguiba/ontologies/2023/9/untitled-ontology-4#> " +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "SELECT * " +
+                "WHERE { " +
+                "?Product rdf:type ns:Product; " +
+                "        ns:name ?name; " +
+                "        ns:description ?description; " +
+                "        ns:date ?date; " +
+                "        ns:price ?price; " +
+                "        ns:quantity ?quantity; " +
+                "        ns:type ?type;";
+
+        if (productName != null && !productName.isEmpty()) {
+            sparqlQuery += " FILTER(regex(?name, \"" + productName + "\", \"i\"))";
+        }
+        if (type != null && !type.isEmpty()) {
+            sparqlQuery += " FILTER(regex(?type, \"" + type + "\", \"i\"))";
+        }
+
+// ORDER BY clause should be placed after all FILTER conditions
+        if (sortBy != null && !sortBy.isEmpty()) {
+            if ("price".equals(sortBy)) {
+                sparqlQuery += " } ORDER BY ?price";
+            } else if ("quantity".equals(sortBy)) {
+                sparqlQuery += " } ORDER BY ?quantity";
+            }
+        }
+
+        sparqlQuery += " }"; // Close the SPARQL query
+        if (model != null) {
+            Model inferredModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+            OutputStream res = JenaEngine.executeQuery(inferredModel, sparqlQuery);
+            System.out.println(res);
+            return res.toString();
+        } else {
+            return "Error when reading model from ontology";
+        }
+    }
+    @GetMapping("/skills")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<String> getSkills(
+            @RequestParam(value = "domain", required = false) String domain,
+            @RequestParam(value = "regexParam", required = false) String regexParam,
+            @RequestParam(value = "orderBy", required = false) String orderBy,
+            @RequestParam(value = "orderType", required = false) String orderType
+    ) {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+            String queryStr = FileManager.get().readWholeFileAsUTF8("data/query_Skill.txt");
+
+
+            // Set the value of ?domainParam
+            if (domain != null && !domain.isEmpty()) {
+                // Replace the parameter placeholder with the actual domain value
+                queryStr = queryStr.replace("?domainParam",  '\"'+domain+'\"' );
+            } else {
+                // If domain is not provided, remove the parameter and the FILTER condition from the query
+                queryStr = queryStr.replace("FILTER (?domainParam != \"\" && ?domain = ?domainParam)", "");
+            }
+
+            if (regexParam != null && !regexParam.isEmpty()) {
+                queryStr = queryStr.replace("?regexParam",  '\"'+regexParam+'\"' );
+            } else {
+                queryStr = queryStr.replace("FILTER regex(?name, ?regexParam, \"i\")", "");
+            }
+
+            if (orderBy != null && !orderBy.isEmpty() && orderType != null && !orderType.isEmpty()
+                    && (orderType.toUpperCase().equals("ASC") || orderType.toUpperCase().equals("DESC"))) {
+                queryStr = queryStr.replace("?orderBy",  '?'+orderBy.toLowerCase() );
+                queryStr = queryStr.replace("?orderType",  orderType.toUpperCase() );
+            } else {
+                queryStr = queryStr.replace("ORDER BY ?orderType(?orderBy)", "");
+            }
+
+            System.out.println(queryStr);
+            // Execute the query
+            Query query = QueryFactory.create(queryStr);
+            QueryExecution qexec = QueryExecutionFactory.create(query, inferedModel);
+
+            // Execute the query
+            ResultSet results = qexec.execSelect();
+
+            JsonArray jsonArray = new JsonArray();
+            while (results.hasNext()) {
+                QuerySolution solution = results.next();
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.add("skill", new JsonPrimitive(solution.get("skill").toString()));
+                jsonObject.add("name", new JsonPrimitive(solution.get("name").toString()));
+                jsonObject.add("domain", new JsonPrimitive(solution.get("domain").toString()));
+                jsonArray.add(jsonObject);
+            }
+
+            // Convert the JSON to a string
+            String jsonResult = jsonArray.toString();
+
+            System.out.println(jsonResult);
+            return new ResponseEntity<>(jsonResult, HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<>("Error when reading model from ontology", HttpStatus.OK);
+        }
+    }
 
 
 
