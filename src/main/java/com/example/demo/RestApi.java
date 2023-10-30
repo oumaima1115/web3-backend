33112f3f826968a42ea154478d934d4d45a2d510
Taher <taher.snoussi@esprit.tn>
2023-10-30 13:54:08 +0100

    filters

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -3,9 +3,15 @@ package com.example.demo;
 
 import java.io.OutputStream;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -100,6 +106,97 @@ public class RestApi {
             return ("Error when reading model from ontology");
         }
 
+    }
+
+    @GetMapping("/jobsSearch")
+    @CrossOrigin(origins = "*")
+    public String getSkills(
+            @RequestParam(value = "domain", required = false) String domain,
+            @RequestParam(value="regexParam", required = false) String regexParam,
+            @RequestParam(value ="orderType",required =false )String orderType,
+            @RequestParam(value = "orderBy",required =false) String orderBy
+
+    )
+
+
+             {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            // query on the model after inference
+            /*OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Skill.txt");
+
+            System.out.println(res);
+            return res.toString();*/
+
+            String queryStr = FileManager.get().readWholeFileAsUTF8("data/query_Job.txt");
+
+
+            // Set the value of ?domainParam
+            if (domain != null && !domain.isEmpty()) {
+                // Replace the parameter placeholder with the actual domain value
+                queryStr = queryStr.replace("?titleParam",  '\"'+domain+'\"' );
+            } else {
+                // If domain is not provided, remove the parameter and the FILTER condition from the query
+                queryStr = queryStr.replace("FILTER (?titleParam != \"\" && ?title = ?titleParam)", "");
+            }
+
+            if (regexParam != null && !regexParam.isEmpty()) {
+                // If regexParam is provided, replace the parameter placeholder
+                queryStr = queryStr.replace("?regexParam", '\"' + regexParam + '\"');
+            } else {
+                // If regexParam is not provided, remove the FILTER condition from the query
+                queryStr = queryStr.replace("FILTER (?regexParam != \"\" && ?description = ?regexParam)", "");
+            }
+
+            if (orderBy != null && !orderBy.isEmpty() && orderType != null && !orderType.isEmpty()
+                    && (orderType.toUpperCase().equals("ASC") || orderType.toUpperCase().equals("DESC"))) {
+                queryStr = queryStr.replace("?orderBy",  '?'+orderBy.toLowerCase() );
+                queryStr = queryStr.replace("?orderType",  orderType.toUpperCase() );
+            } else {
+                queryStr = queryStr.replace("ORDER BY ?orderType(?orderBy)", "");
+            }
+
+            System.out.println(queryStr);
+            // Execute the query
+            Query query = QueryFactory.create(queryStr);
+            QueryExecution qexec = QueryExecutionFactory.create(query, inferedModel);
+
+            // Execute the query
+            ResultSet results = qexec.execSelect();
+
+            JsonArray jsonArray = new JsonArray();
+            while (results.hasNext()) {
+                QuerySolution solution = results.next();
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.add("Job", new JsonPrimitive(solution.get("Job").toString()));
+                jsonObject.add("title", new JsonPrimitive(solution.get("title").toString()));
+                jsonObject.add("description", new JsonPrimitive(solution.get("description").toString()));
+                jsonObject.add("type", new JsonPrimitive(solution.get("type").toString()));
+                jsonObject.add("address", new JsonPrimitive(solution.get("address").toString()));
+                jsonObject.add("salary_range", new JsonPrimitive(solution.get("salary_range").toString()));
+
+
+
+
+                jsonArray.add(jsonObject);
+            }
+
+            // Convert the JSON to a string
+            String jsonResult = jsonArray.toString();
+
+            System.out.println(jsonResult);
+            return jsonResult;
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
     }
 
 
