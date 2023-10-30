61ce0c2cc925cc587a1a1b2f20989462b31d16b9
Taher <taher.snoussi@esprit.tn>
2023-10-30 19:05:09 +0100

    filter

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class RestApi {
 
     Model model = JenaEngine.readModel("data/inclusify.owl");
@@ -117,7 +118,6 @@ public class RestApi {
             @RequestParam(value = "orderBy",required =false) String orderBy
 
     )
-
 
              {
         String NS = "";
